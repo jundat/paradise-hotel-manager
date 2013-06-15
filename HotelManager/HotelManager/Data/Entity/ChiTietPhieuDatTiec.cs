115c5f224350e18995133a8ae9bce8c2035499dd
longpham.uit <longpham.uit@4b01cef7-59e9-83b3-5f08-85f3805627a9>
2013-06-15 02:07:31 +0000

    Đã thêm đủ 8 Entity cuối!

---
+++ HotelManager/HotelManager/Data/Entity/ChiTietPhieuDatTiec.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelManager.Data.Entity
+{
+    class ChiTietPhieuDatTiec
+    {
+        public int MaChiTietPhieuDatTiec;
+        public int MaPhieuDatTiec;
+        public string TenMon;
+        public float DonGia;
+        public int SoLuong;
+        public string YeuCau;
+
+        public ChiTietPhieuDatTiec(int machitiet, int maphieu, string tenmon, float dongia, int soluong, string yeucau)
+        {
+            MaChiTietPhieuDatTiec = machitiet;
+            MaPhieuDatTiec = maphieu;
+            TenMon = tenmon;
+            DonGia = dongia;
+            SoLuong = soluong;
+            YeuCau = yeucau;
+        }
+    }
+}
--- HotelManager/HotelManager/Data/Entity/PhieuDatTiec.cs
+++ HotelManager/HotelManager/Data/Entity/PhieuDatTiec.cs
@@ -12,6 +12,16 @@ namespace HotelManager.Data.Entity
         public int MaPhong;
         public DateTime ThoiDiem;
         public float TongTien;
+        public bool TinhTrangThanhToan;
 
+        public PhieuDatTiec(int maphieudattiec, string tenkhach, int maphong, DateTime thoidiem, float tongtien, bool tinhtrang)
+        {
+            MaPhieuDatTiec = maphieudattiec;
+            TenKhach = tenkhach;
+            MaPhong = maphong;
+            ThoiDiem = thoidiem;
+            TongTien = tongtien;
+            TinhTrangThanhToan = tinhtrang;
+        }
     }
 }
