c43f55e26cb765a6801dfb956f1f809502120174
LeAnh.K5SE.UIT <LeAnh.K5SE.UIT@gmail.com>
2013-06-17 03:25:08 +0000

    Thêm 8 Class Bus

---
+++ HotelManager/HotelManager/Business/BusBangKe.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Collections;
+using HotelManager.Data;
+using HotelManager.Data.Entity;
+
+namespace HotelManager.Business
+{
+    class BusBangKe
+    {
+        /// <summary>
+        /// Lấy danh sách các bảng kê dưới dạng List
+        /// </summary>
+        /// <returns></returns>
+        public static ArrayList GetList()
+        {
+            return DataBangKe.GetList();
+        }
+
+        /// <summary>
+        /// Lấy hết bảng BANG_KE
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GetTable()
+        {
+            return DataBangKe.GetTable();
+        }
+
+        /// <summary>
+        /// Update giá trị mới cho bảng BANG_KE
+        /// </summary>
+        /// <param name="dataTable"></param>
+        public static void UpdateTable(DataTable dataTable)
+        {
+            DataBangKe.UpdateTable(dataTable);
+        }
+
+        /// <summary>
+        /// Thêm Bảng Kê mới
+        /// </summary>
+        /// <param name="_phieu"></param>
+        /// <returns>Trả về MaBangKE nếu thành công, ngược lại -1</returns>
+        public static int Add(BangKe _bangKe)
+        {
+            return DataBangKe.Add(_bangKe);
+        }
+
+        /// <summary>
+        /// Xoá một Bảng kê
+        /// </summary>
+        /// <param name="_maBangKe"></param>
+        public static void Delete(int _maBangKe)
+        {
+            DataBangKe.Delete(_maBangKe);
+        }
+
+        /// <summary>
+        /// Sửa 1 Loại Phòng
+        /// </summary>
+        /// <param name="_phieu"></param>
+        public static void UpdateLoaiPhong(BangKe _bangKe)
+        {
+            DataBangKe.UpdateLoaiPhong(_bangKe);
+        }
+
+        /// <summary>
+        /// Tìm kiêm một Bảng kê thông qua Mã bảng kê
+        /// </summary>
+        /// <param name="_maLoaiPhong"></param>
+        /// <returns></returns>
+        public static BangKe Find(int _maBangKe)
+        {
+            return DataBangKe.Find(_maBangKe);
+        }
+        
+
+    }
+}
---
+++ HotelManager/HotelManager/Business/BusQuyDinh.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Collections;
+using HotelManager.Data;
+using HotelManager.Data.Entity;
+
+namespace HotelManager.Business
+{
+    class BusQuyDinh
+    {
+        /// <summary>
+        /// Đặt Số khách tối đa trong một phòng
+        /// </summary>
+        /// <returns></returns>
+        public static int GetSoKhachToiDaTrongMotPhong()
+        {
+            return DataQuyDinh.GetSoKhachToiDaTrongMotPhong();
+        }
+
+        /// <summary>
+        /// Cập nhật Số khách tối đa trong một phòng
+        /// </summary>
+        /// <param name="_soKhachToiDaTrongMotPhong"></param>
+        public static void UpdateSoKhachToiDaTrongMotPhong(int _soKhachToiDaTrongMotPhong)
+        {
+            DataQuyDinh.UpdateSoKhachToiDaTrongMotPhong(_soKhachToiDaTrongMotPhong);
+        }
+
+         /// <summary>
+        /// Đặt Tỷ lệ cọc
+        /// </summary>
+        /// <returns></returns>
+        public static float GetTyLeCoc()
+        {
+            return DataQuyDinh.GetTyLeCoc();
+        }
+
+        /// <summary>
+        /// Đặt Số giờ thuê với giá gốc
+        /// </summary>
+        /// <returns></returns>
+        public static int GetSoGioThueVoiGiaGoc()
+        {
+            return DataQuyDinh.GetSoGioThueVoiGiaGoc();
+        }
+        
+        /// <summary>
+        /// Cập nhật Số giờ thuê với giá gốc
+        /// </summary>
+        /// <param name="_soGioThueVoiGiaGoc"></param>
+        public static void UpdateSoGioThueVoiGiaGoc(int _soGioThueVoiGiaGoc)
+        {
+            DataQuyDinh.UpdateSoGioThueVoiGiaGoc(_soGioThueVoiGiaGoc);
+        }
+
+        /// <summary>
+        /// Đặt Tỷ lệ giá phòng nếu thuê theo ngày
+        /// </summary>
+        /// <returns></returns>
+        public static float GetTyLeGiaPhongNeuThueTheoNgay()
+        {
+            return DataQuyDinh.GetTyLeGiaPhongNeuThueTheoNgay();
+        }
+
+        /// <summary>
+        /// Cập nhật Tỷ lệ giá phòng nếu thuê theo ngày
+        /// </summary>
+        /// <param name="_tyLeGiaPhongNeuThueTheoNgay"></param>
+        public static void UpdateTyLeGiaPhongNeuThueTheoNgay(float _tyLeGiaPhongNeuThueTheoNgay)
+        {
+            DataQuyDinh.UpdateTyLeGiaPhongNeuThueTheoNgay(_tyLeGiaPhongNeuThueTheoNgay);
+        }
+
+        /// <summary>
+        /// Lấy thông số của quy định thông qua đối tượng quy định
+        /// </summary>
+        /// <returns></returns>
+        public static QuyDinh GetQuyDinh()
+        {
+            return DataQuyDinh.GetQuyDinh();
+        }
+
+        /// <summary>
+        /// Cập nhật thông số của quy định thông qua đối tượng quy định
+        /// </summary>
+        /// <param name="_quyDinh"></param>
+        public static void UpdateQuyDinh(QuyDinh _quyDinh)
+        {
+            DataQuyDinh.UpdateQuyDinh(_quyDinh);
+        }
+
+
+    }
+}
